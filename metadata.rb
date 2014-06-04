16664d92502e105f6d1409ad63869c9581659608
Ian Duffy <ian@ianduffy.ie>
2014-06-04 21:40:34 +0100

    init

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cloudstack'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures cloudstack'
+long_description 'Installs/Configures cloudstack'
+version          '0.1.0'
+
