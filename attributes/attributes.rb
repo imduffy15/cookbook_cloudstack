5cd1c5f52832f547fd84b2624b36baab37191b22
Ian Duffy <ian@ianduffy.ie>
2014-09-24 00:58:26 +0100

    Update attributes.rb

--- attributes/attributes.rb
+++ attributes/attributes.rb
@@ -32,7 +32,7 @@ default['cloudstack']['storage']['secondary'] = '/exports/secondary'
 default['cloudstack']['systemvms'] = [
     {
         'hypervisor' => 'XenServer',
-        'url' => 'http://jenkins.buildacloud.org/job/build-systemvm-master/lastSuccessfulBuild/artifact/tools/appliance/dist/systemvmtemplate-master-xen.vhd.bz2',
+        'url' => 'http://jenkins.buildacloud.org/job/build-systemvm64-master/lastSuccessfulBuild/artifact/tools/appliance/dist/systemvm64template-master-421-xen.vhd.bz2',
     }
 ]
 
