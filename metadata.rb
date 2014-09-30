2e69e8ea80d80df10bee9f790f637dc198d96ef1
imduffy15 <ian@ianduffy.ie>
2014-09-30 23:21:38 +0100

    version jump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'duffy@apache.org'
 license 'Apache 2.0'
 description 'Installs/Configures cloudstack'
 long_description 'Installs/Configures cloudstack'
-version '0.0.1'
+version '0.0.5'
 
 depends 'mysql'
 depends 'co-nfs'
