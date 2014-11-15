a0a25c6bc6207645c4057ee974841d7447c4a4e1
Ian Duffy <ian@ianduffy.ie>
2014-11-15 22:23:46 +0000

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'duffy@apache.org'
 license 'Apache 2.0'
 description 'Installs/Configures cloudstack'
 long_description 'Installs/Configures cloudstack'
-version '0.0.7'
+version '0.0.8'
 
 depends 'mysql'
 depends 'co-nfs'
